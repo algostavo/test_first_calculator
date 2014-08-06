09a6ddad44dfce2f48bd1ef0c7dccb37bb2886f5
algostavo <algostavo@gmail.com>
2014-08-06 12:15:49 -0400

    test methods for multiply and divide

--- app/calculator.rb
+++ app/calculator.rb
@@ -8,4 +8,11 @@ class Calculator
     x - y
   end
   
+  def multiply(x,y)
+    x * y
+  end
+  
+  def divide(x,y)
+    x / y
+  end
 end
--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -13,4 +13,16 @@ class CalculatorTest < Test::Unit::TestCase
     actual = calculator.subtract(5, 3)
     assert_equal(2, actual)
   end 
+  
+  def test_can_multiply
+    calculator = Calculator.new
+    actual = calculator.multiply(4, 6)
+    assert_equal(24, actual)
+  end
+  
+  def test_can_divide
+    calculator = Calculator.new
+    actual = calculator.divide(10, 5)
+    assert_equal(2, actual)
+  end
 end
