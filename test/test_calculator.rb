0ad8f9c6e2ddf701a35432f9f53ed12046a39af7
algostavo <algostavo@gmail.com>
2014-08-06 12:22:23 -0400

    setup method included

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -2,27 +2,28 @@ require 'test/unit'
 require 'app/calculator'
 
 class CalculatorTest < Test::Unit::TestCase
+  
+  def setup
+    @calculator = Calculator.new
+  end
+  
   def test_can_add
-    calculator = Calculator.new
-    actual = calculator.add(2, 2)
+    actual = @calculator.add(2, 2)
     assert_equal(4, actual)
   end 
   
   def test_can_subtract
-    calculator = Calculator.new
-    actual = calculator.subtract(5, 3)
+    actual = @calculator.subtract(5, 3)
     assert_equal(2, actual)
   end 
   
   def test_can_multiply
-    calculator = Calculator.new
-    actual = calculator.multiply(4, 6)
+    actual = @calculator.multiply(4, 6)
     assert_equal(24, actual)
   end
   
   def test_can_divide
-    calculator = Calculator.new
-    actual = calculator.divide(10, 5)
+    actual = @calculator.divide(10, 5)
     assert_equal(2, actual)
   end
 end
